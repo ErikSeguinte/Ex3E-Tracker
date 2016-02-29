10475cc10466684bcc122b0240dfa6ad64bfd846
Erik Seguinte <ErikSeguinte@gmail.com>
2016-02-28 20:47:50 -0800

    added tracker.cpp

---
+++ tracker.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip>
+#include<fstream>
+#include<string>
+#include<vector>
+#include "Character.h"
+
+int main()
+{
+    std::cout << "Hello World";
+    return 0;
+}
