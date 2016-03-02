434968dfedbe719fef5cafc8fc4a152d87dc530b
Erik Seguinte <ErikSeguinte@gmail.com>
2016-03-02 11:13:11 -0800

    Preparing to use QT

--- gui.cpp
+++ gui.cpp
@@ -5,11 +5,8 @@ int main(int argc, char *argv[]) {
         
         QApplication app(argc, argv);
 
-	    QWidget window;
-
-	        window.resize(250, 150);
-		    window.setWindowTitle("Simple example");
-		        window.show();
-
-			    return app.exec();
+	QMainWindow window;
+	Ui_MainWindow mainwindow();	
+	window = mainwindow;
+	return app.exec();
 }
