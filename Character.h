c643ca9f9cf8eb1002643adaf238c1ff16247154
Erik Seguinte <ErikSeguinte@gmail.com>
2016-03-01 17:25:42 -0800

    Getters and Setters for Characters

--- Character.h
+++ Character.h
@@ -6,16 +6,25 @@ class Character
 {
     private:
 	std::string name;
-    	int initiative;
+	int initiative;
 	bool is_crashed;
 	bool has_gone;
 	Character *shift_target;
 	int onslaught;
 	bool is_delayed;
     public:
+
+	//Constructors
 	Character(std::string);
-	std::string get_name()
-	{ return name;}
+	
+	//Getters
+	std::string get_name();
+	int get_int();
+	bool get_crashed();
+	bool get_has_gone();
+	Character shift_target();
+
+	//Setters
 
 };
 #endif
